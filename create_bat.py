68b0607dd486f579d7aa93a7fb5f2b8ed7595eb6
miu <garnet200521358@gmail.com>
2023-11-17 02:35:51 +0900

    mlib更新

--- create_bat.py
+++ create_bat.py
@@ -1,26 +1,29 @@
-from executor import APP_NAME, VERSION_NAME
+import sys
+
+sys.path.append("src")
+from executor import APP_NAME, VERSION_NAME  # noqa: E402
 
 EXE_NAME = f"{APP_NAME}_{VERSION_NAME}"
 
 for file_path, args in [
     (
-        f"../dist/{EXE_NAME}.exe - en.bat",
+        f"dist/{EXE_NAME}.exe - en.bat",
         "--verbose 20 --out_log 0 --lang en-us",
     ),
     (
-        f"../dist/{EXE_NAME}.exe - zh.bat",
+        f"dist/{EXE_NAME}.exe - zh.bat",
         "--verbose 20 --out_log 0 --lang zh",
     ),
     (
-        f"../dist/{EXE_NAME}.exe - ko.bat",
+        f"dist/{EXE_NAME}.exe - ko.bat",
         "--verbose 20 --out_log 0 --lang ko",
     ),
     (
-        f"../dist/{EXE_NAME}.exe - デバッグ版.bat",
+        f"dist/{EXE_NAME}.exe - デバッグ版.bat",
         "--verbose 10 --out_log 1 --lang ja",
     ),
     (
-        f"../dist/{EXE_NAME}.exe - ログあり版.bat",
+        f"dist/{EXE_NAME}.exe - ログあり版.bat",
         "--verbose 20 --out_log 1 --lang ja",
     ),
 ]:
